1cdb1694345ea94ed7520739f3c1863632c26080
Megan Sharon <615329+mgnsharon@users.noreply.github.com>
2024-04-24 01:16:29 -0400

    third challenge

--- app/server.go
+++ app/server.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"net"
 	"os"
 )
+
+type Request struct {
+	Method string
+	Path string
+	Version string
+}
 
 func main() {
 	
@@ -13,17 +20,45 @@ func main() {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+	fmt.Printf("Listening on %s", l.Addr())
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
 	}
 
-	_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	b, err := conn.Read(buf)
 	if err != nil {
-		fmt.Println("Error writing to connection: ", err.Error())
+		fmt.Println("Error reading: ", err.Error())
 		os.Exit(1)
+	}
+	data := bytes.Split(buf[:b], []byte("\r\n"))
+	sl := bytes.Split(data[0], []byte(" "))
+	req := Request{
+		Method: string(sl[0]),
+		Path: string(sl[1]),
+		Version: string(sl[2]),
+	}
+	if req.Path == "/" {
+		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
+	} else {
+		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		if err != nil {
+			fmt.Println("Error writing to connection: ", err.Error())
+			os.Exit(1)
+		}
 	}
 	
 }
