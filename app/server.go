82b86dead3027efc1c331f574231928cd03e45fd
Megan Sharon <615329+mgnsharon@users.noreply.github.com>
2024-04-24 11:50:19 -0400

    eighth challenge

--- app/server.go
+++ app/server.go
@@ -17,6 +17,13 @@ type Request struct {
 	Body []byte
 }
 
+type Response struct {
+	Version string
+	Status string
+	Headers map[string]string
+	Body []byte
+}
+
 var dir = flag.String("directory", ".", "Directory to serve files from")
 
 func main() {
@@ -40,15 +47,13 @@ func main() {
 
 }
 
-func handleRequest(conn net.Conn) {
-	defer conn.Close()
+func parseRequest(conn net.Conn) Request {
 	buf := make([]byte, 1024)
 	b, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println("Error reading: ", err.Error())
 		os.Exit(1)
 	}
-
 	data := bytes.Split(buf[:b], []byte("\r\n\r\n"))
 	header := bytes.Split(data[0], []byte("\r\n"))
 	reqbody := data[1]
@@ -67,53 +72,84 @@ func handleRequest(conn net.Conn) {
 		hh := bytes.Split(h, []byte(": "))
 		req.Headers[string(hh[0])] = string(hh[1])
 	}
+	return req
+}
+
+func handleRequest(conn net.Conn) {
+	defer conn.Close()
+	
+	req := parseRequest(conn)
+	
+	if req.Method == "GET" {
+		res := handleGetRequest(req)
+		writeResponse(conn, res)
+	} else if req.Method == "POST" {
+		res := handlePostRequest(req)
+		writeResponse(conn, res)
+	} else {
+		res := Response{ Version: "HTTP/1.1", Status: "405 Method Not Allowed"}
+		writeResponse(conn, res)
+	}
+	
+}
+
+func handleGetRequest(req Request) Response {
 	if req.Path == "/" {
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
-		}
+		return Response{ Version: "HTTP/1.1", Status: "200 OK"}
 	} else if strings.HasPrefix(req.Path, "/files/") {
 		file := strings.TrimPrefix(req.Path, "/files/")
-		fmt.Println("File: ", file)
 		fp := fmt.Sprint(*dir, string(os.PathSeparator), file)
-		fmt.Println("File Path: ", fp)
 		f, err := os.ReadFile(fp)
 		if err != nil {
-			_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-			if err != nil {
-				fmt.Println("Error writing to connection: ", err.Error())
-				os.Exit(1)
-			}
-			return
+			return Response{ Version: "HTTP/1.1", Status: "404 Not Found"}
 		}
-		 
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Length: " +  fmt.Sprintf("%d\r\n", len(f)) +"Content-Type: application/octet-stream\r\n\r\n" + string(f)))
-		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
-		}
+		return Response{ Version: "HTTP/1.1", Status: "200 OK", Headers: map[string]string{"Content-Length": fmt.Sprintf("%d", len(f)), "Content-Type": "application/octet-stream"}, Body: f}
 		
 	} else if strings.HasPrefix(req.Path, "/echo/"){
 		body := strings.TrimPrefix(req.Path, "/echo/")
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", len(body)) + "\r\n\r\n" + body))
-		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
-		}
+		return Response{ Version: "HTTP/1.1", Status: "200 OK", Headers: map[string]string{"Content-Length": fmt.Sprintf("%d", len(body)), "Content-Type": "text/plain"}, Body: []byte(body)}
 	} else if strings.HasPrefix(req.Path, "/user-agent") {
-		ua := req.Headers["User-Agent"] 
-		_, err = conn.Write([]byte("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + fmt.Sprintf("%d", len(ua)) + "\r\n\r\n" + ua))
-			if err != nil {
-				fmt.Println("Error writing to connection: ", err.Error())
-				os.Exit(1)
-			}
+		ua := req.Headers["User-Agent"]
+		return Response{ Version: "HTTP/1.1", Status: "200 OK", Headers: map[string]string{"Content-Length": fmt.Sprintf("%d", len(ua)), "Content-Type": "text/plain"}, Body: []byte(ua)}
 	} else {
-		_, err = conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		return Response{ Version: "HTTP/1.1", Status: "404 Not Found"}
+	}
+}
+
+func handlePostRequest(req Request) Response {
+	if strings.HasPrefix(req.Path, "/files/") {
+		fname := strings.TrimPrefix(req.Path, "/files/")
+		fp := fmt.Sprint(*dir, string(os.PathSeparator), fname)
+		err := os.WriteFile(fp, req.Body, 0644)
+		if err != nil {
+			return Response{ Version: "HTTP/1.1", Status: "500 Internal Server Error"}
+		}
+	}
+	
+	return Response{ Version: "HTTP/1.1", Status: "201 Created"}
+}
+
+func writeResponse(conn net.Conn, res Response) {
+	_, err := conn.Write([]byte(res.Version + " " + res.Status + "\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+	for k, v := range res.Headers {
+		_, err := conn.Write([]byte(k + ": " + v + "\r\n"))
 		if err != nil {
 			fmt.Println("Error writing to connection: ", err.Error())
 			os.Exit(1)
 		}
 	}
-	
+	_, err = conn.Write([]byte("\r\n"))
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
+	_, err = conn.Write(res.Body)
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		os.Exit(1)
+	}
 }
